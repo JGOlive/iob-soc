7902d46f7112e7f38d4416a8fa6ca6976e4d4caf
José <jose.de.oliveira@tecnico.ulisboa.pt>
2022-11-29 17:29:13 +0000

    Fibonacci sequence
    20 iterations

--- software/firmware/firmware.c
+++ software/firmware/firmware.c
@@ -3,13 +3,10 @@
 #include "iob-uart.h"
 #include "printf.h"
 
-char *send_string = "Sending this string as a file to console.\n"
-                    "The file is then requested back from console.\n"
-                    "The sent file is compared to the received file to confirm " 
-                    "correct file transfer via UART using console.\n"
-                    "Generating the file in the firmware creates an uniform "
-                    "file transfer between pc-emul, simulation and fpga without"
-                    " adding extra targets for file generation.\n";
+#define ITERATIONS 20 
+#define MAX_STR_SIZE 200
+
+char send_string[200];
 
 // copy src to dst
 // return number of copied chars (excluding '\0')
@@ -39,8 +36,56 @@ int compare_str(char *str1, char *str2, int str_size) {
     return 0;
 }
 
+void generate_fibonacci_nums(int *number_array, int iter)
+{
+    int i;
+
+    number_array[0] = 0;
+    number_array[1] = 1;
+
+    for (i = 2; i < iter; i++){
+    number_array[i] = number_array[i-1] + number_array[i-2];
+    }
+
+return;
+}
+
+void generate_string(int *numbers, char* total_string, int iter){
+
+    int i,decimal,n_numbers, str_idw = 20, num_test;
+    char *number_string[10];
+    char test_string[MAX_STR_SIZE] = "Fibonacci sequence:\n"; /*20 + \0*/
+    char *space =" "; 
+    
+    for(i = 0; i< ITERATIONS; i++){
+        decimal = 1;
+        n_numbers = 0;
+        while(numbers[i]/decimal >= 10){
+            decimal = decimal*10;
+            n_numbers = 1 + n_numbers;
+        }
+        num_test = numbers[i];
+        while (n_numbers >= 0){
+            test_string[str_idw] = '0'+ num_test/decimal;
+            num_test = num_test % decimal;
+            decimal = decimal/10;
+            str_idw = str_idw + 1;
+            n_numbers = n_numbers - 1;
+        }
+        test_string[str_idw] = ' ';
+        str_idw = str_idw + 1;
+    }
+    test_string[str_idw] = '\n';
+    test_string[str_idw + 1] = '\0';
+
+    string_copy(total_string,test_string);
+    return;
+}
+
 int main()
 {
+
+  int numbers_int[ITERATIONS];
   //init uart
   uart_init(UART_BASE,FREQ/BAUD);
 
@@ -48,7 +93,11 @@ int main()
   uart_puts("\n\n\nHello world!\n\n\n");
 
   //test printf with floats 
-  printf("Value of Pi = %f\n\n", 3.1415);
+  generate_fibonacci_nums(numbers_int, ITERATIONS);
+
+  generate_string(numbers_int,send_string, ITERATIONS); 
+   
+  printf("%s\n", send_string);
 
   //test file send
   char *sendfile = malloc(1000);
